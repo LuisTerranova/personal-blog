1f3b65b0e754ffa6c83c4ad2f54b3ac94835eeda
LuisTerranova <linharesdev43@gmail.com>
2025-07-14 17:54:54 -0400

    Creation of base models

---
+++ personal-blog.core/Models/Post.cs
@@ -0,0 +1,13 @@
+namespace personal_blog.core.Models;
+
+public class Post
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public DateTime Created { get; set; } =  DateTime.Now;
+    public DateTime Updated { get; set; } = DateTime.Now;
+    public string Category { get; set; }
+
+    public long UserId { get; set; }
+}
---
+++ personal-blog.core/Models/Project.cs
@@ -0,0 +1,13 @@
+namespace personal_blog.core.Models;
+
+public class Project
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public string RepoLink { get; set; }
+    public DateTime Created { get; set; }
+
+    public long UserId { get; set; }
+}
